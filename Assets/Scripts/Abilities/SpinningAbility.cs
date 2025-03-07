49184cc9f431aae2aee83b86dc79f0d70eb71edd
Philip Heilmair <philip.heilmair@gmail.com>
2025-03-07 15:31:01 +0900

    added free version character of SPUM package

--- Assets/Scripts/Abilities/SpinningAbility.cs
+++ Assets/Scripts/Abilities/SpinningAbility.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using Units;
 using UnityEngine;
 
@@ -9,8 +10,12 @@ namespace Abilities
 
         private void Start()
         {
+            transform.position = PlayerUnit.Instance.transform.position;
+            transform.DOMove(PlayerUnit.Instance.transform.position + Offset * Vector3.right, .25f)
+                .SetEase(Ease.OutBounce);
             transform.parent = PlayerUnit.Instance.transform;
-            transform.position = new Vector3(transform.position.x + Offset, transform.position.y, transform.position.z);
+            // transform.position = new Vector3(transform.position.x + Offset, transform.position.y, transform.position.z);
+            // transform.DOMove(transform.position + Offset * Vector3.right, .25f).SetEase(Ease.OutBounce);
         }
 
         private void Update()
